5d922944ce00a90d1bf436d7b61bc02ba6b3fb1e
Edgar-Loop <mb7c5hz2zm@privaterelay.appleid.com>
2026-07-13 21:23:04 -0700

    tweaks
    LopezEdgar_PolishedLighting_01
    I have tweaked the lighting a bit to fit the style I want

--- Scenes/rocket.gd
+++ Scenes/rocket.gd
@@ -9,18 +9,12 @@ func _ready():
 	
 func _physics_process(delta):
 	global_position += -global_transform.basis.z * speed * delta
-	move_and_slide()
-	for collision in get_slide_collision_count():
-			var hit = get_slide_collision(collision).get_collider()
-			
-			if hit.is_in_group("Env"):
-				print("Hit")
-				queue_free()
-			
+
 func _on_timer_timeout() -> void:
 	get_parent().queue_free()
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if body.is_in_group("Env"):
-		print("Hit")
+	#if body.is_in_group("Env"):
+		print(body.name)
+		get_parent().queue_free()
